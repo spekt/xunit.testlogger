c5807953ee63c3330ec2234c7a3afaa20cd130e8
Arun Mahapatra <pratikarun@gmail.com>
2018-06-09 18:19:27 +0530

    fix: fix logger arguments. Add diagnostics to tests.

--- test/Xunit.Xml.TestLogger.AcceptanceTests/DotnetTestFixture.cs
+++ test/Xunit.Xml.TestLogger.AcceptanceTests/DotnetTestFixture.cs
@@ -9,7 +9,25 @@ namespace Xunit.Xml.TestLogger.AcceptanceTests
         public DotnetTestFixture()
         {
             var testProject = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "..", "Xunit.Xml.TestLogger.NetCore.Tests"));
-            var testLogger = $"--logger 'xunit;LogFilePath=test-results.xml'";
+            var testLogger = $"--logger:\"xunit;LogFilePath=test-results.xml\"";
+
+            // Delete stale results file
+            var testLogFile = Path.Combine(testProject, "test-results.xml");
+            if (File.Exists(testLogFile))
+            {
+                File.Delete(testLogFile);
+            }
+
+            // Log the contents of test output directory. Useful to verify if the logger is copied
+            Console.WriteLine("------------");
+            Console.WriteLine("Contents of test output directory:");
+            foreach (var f in Directory.GetFiles(Path.Combine(testProject, "bin/Debug/netcoreapp2.0")))
+            {
+                Console.WriteLine("  " + f);
+            }
+            Console.WriteLine();
+
+            // Run dotnet test with logger
             using (var p = new Process())
             {
                 p.StartInfo.UseShellExecute = false;
@@ -18,11 +36,10 @@ namespace Xunit.Xml.TestLogger.AcceptanceTests
                 p.StartInfo.Arguments = $"test --no-build {testLogger} {testProject}";
                 p.Start();
 
+                Console.WriteLine("dotnet arguments: " + p.StartInfo.Arguments);
                 // To avoid deadlocks, always read the output stream first and then wait.
                 string output = p.StandardOutput.ReadToEnd();
                 p.WaitForExit();
-                Console.WriteLine("------------");
-                Console.WriteLine("dotnet arguments: " + p.StartInfo.Arguments);
                 Console.WriteLine("dotnet output: " + output);
                 Console.WriteLine("------------");
             }
